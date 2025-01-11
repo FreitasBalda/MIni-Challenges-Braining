d1ffb125a56991e4a1089c90dd69cb54e752b6b2
Gonçalo Freitas <160952799+FreitasBalda@users.noreply.github.com>
2025-01-11 13:56:53 -0800

    patients.py

--- Python/patients.py
+++ Python/patients.py
@@ -1,17 +1,22 @@
 import pandas as pd
 
+# Load patient data
 df = pd.read_csv('patient_data.csv')
 print(df.head(5))
 
+# Calculate average BMI
 avgbmi = df["bmi"].mean()
-print(f"The average bmi is {avgbmi:.2f}kg/m²")
+print(f"The average BMI is {avgbmi:.2f} kg/m²")
 
-hbp_count = df[df["blood_pressure"]>140].shape[0]
-print(f"The number patients with high blood pressure is (bp > 140mmHg):{hbp_count}")
+# Count patients with high blood pressure
+hbp_count = df[df["blood_pressure"] > 140].shape[0]
+print(f"The number of patients with high blood pressure (BP > 140 mmHg): {hbp_count}")
 
+# Find the oldest patient
 opa = df["age"].max()
-print(f"The oldest patient age is:{opa}")
+print(f"The oldest patient's age is: {opa}")
 
+# Categorize patients
 def categorize_patient(row):
     if 18.5 <= row["bmi"] <= 24.9 and row["blood_pressure"] <= 140:
         return "Healthy"
@@ -20,6 +25,6 @@ def categorize_patient(row):
 
 df["Category"] = df.apply(categorize_patient, axis=1)
 
+# Save processed data
 df.to_csv("processed_patient_data.csv", index=False)
 print("Processed data has been saved to 'processed_patient_data.csv'.")
-
